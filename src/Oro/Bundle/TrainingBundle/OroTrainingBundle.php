52053093fefd1f9c5c9218ecf17de0d82b7d4745
Wieworki <agussb52@gmail.com>
2023-12-27 11:36:18 -0300

    Adding inventory status to PDP

--- src/Oro/Bundle/TrainingBundle/OroTrainingBundle.php
+++ src/Oro/Bundle/TrainingBundle/OroTrainingBundle.php
@@ -5,8 +5,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OroTrainingBundle extends Bundle
 {
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
 }
+

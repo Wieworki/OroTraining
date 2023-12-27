c30435ad324a5c970f4dd5def4075037d3c3b291
Wieworki <agussb52@gmail.com>
2023-12-27 17:57:30 -0300

    Improving definition of layout changes

---
+++ src/AAXIS/Bundle/TrainingBundle/AAXISTrainingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AAXIS\Bundle\TrainingBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AAXISTrainingBundle extends Bundle
+{
+}
+
--- src/Oro/Bundle/TrainingBundle/OroTrainingBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Oro\Bundle\TrainingBundle;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class OroTrainingBundle extends Bundle
-{
-}
-
